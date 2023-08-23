6747d0fbae5206e5042a8c1582959077844588d0
Lasindu Anjana <lasindua@gmail.com>
2023-08-24 00:38:17 +0530

    Updates

---
+++ precache-manifest.16b63f56e6ab0103a1f7138f750d2302.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d54a2b18aa53292a876",
+    "url": "/static/css/main.ad2c269e.chunk.css"
+  },
+  {
+    "revision": "2d54a2b18aa53292a876",
+    "url": "/static/js/main.2d54a2b1.chunk.js"
+  },
+  {
+    "revision": "2074423f1ea920086600",
+    "url": "/static/js/1.2074423f.chunk.js"
+  },
+  {
+    "revision": "2105b5a64eae01997a77",
+    "url": "/static/css/2.c12aba11.chunk.css"
+  },
+  {
+    "revision": "2105b5a64eae01997a77",
+    "url": "/static/js/2.2105b5a6.chunk.js"
+  },
+  {
+    "revision": "d4a1a7a4680295180dcc",
+    "url": "/static/js/runtime~main.d4a1a7a4.js"
+  },
+  {
+    "revision": "1479ea61d1d0e1fae2dc32211c587fdb",
+    "url": "/static/media/20200306_160034-01.1479ea61.jpeg"
+  },
+  {
+    "revision": "2d2a8eb0bf66aac19fec884eaa9138c8",
+    "url": "/static/media/CV_Lasindu_Anjana.2d2a8eb0.pdf"
+  },
+  {
+    "revision": "42c201d8eefd69708ac59187f516187a",
+    "url": "/static/media/photo_2023-08-23_19-33-15.42c201d8.jpg"
+  },
+  {
+    "revision": "df5a83cda67ee63603a441868c24579a",
+    "url": "/static/media/projectImage1.df5a83cd.png"
+  },
+  {
+    "revision": "541187970fe45d25061904070394d697",
+    "url": "/static/media/projectImage2.54118797.png"
+  },
+  {
+    "revision": "177f8899d561f237b04e711f5692b9b9",
+    "url": "/static/media/projectImage3.177f8899.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "3cca9579b91cb075d04889e05706f86a",
+    "url": "/index.html"
+  }
+];
